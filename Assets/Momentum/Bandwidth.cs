7ecabcb8505efd7e3bcb3f1b8f92009f94767bda
Paul Pacheco <paul.pacheco@aa.com>
2021-02-08 16:17:07 -0600

    feat: smooth out bandwidth

--- Assets/Momentum/Bandwidth.cs
+++ Assets/Momentum/Bandwidth.cs
@@ -7,12 +7,20 @@ using System;
 
 public class Bandwidth : MonoBehaviour
 {
-    
+    ExponentialMovingAverage receiveAverage = new ExponentialMovingAverage(30);
+    ExponentialMovingAverage sendAverage = new ExponentialMovingAverage(30);
+
     public Transport transport;
     public Text text;
 
+    [Tooltip("How many seconds to consider when calculating the average bandwidth")]
+    [Range(1,30)]
+    public int window = 5;
+
     public void Start()
     {
+        receiveAverage = new ExponentialMovingAverage(window);
+
         StartCoroutine(Refresh());
     }
 
@@ -31,26 +39,29 @@ public class Bandwidth : MonoBehaviour
             long receivedDiff = receivedBytes - prevReceivedBytes;
             long sentDiff = sentBytes - prevSentBytes;
 
+            receiveAverage.Add(receivedDiff);
+            sendAverage.Add(sentDiff);
+
             prevReceivedBytes = receivedBytes;
             prevSentBytes = sentBytes;
-            text.text = $"↓{Format(receivedDiff)} ↑{Format(sentDiff)}";
+            text.text = $"↓{Format(receiveAverage.Value)} ↑{Format(sendAverage.Value)}";
         }
     }
 
-    private string Format(long bytesPerSecond)
+    private string Format(double bytesPerSecond)
     {
-        long bitsPerSecond = bytesPerSecond * 8;
+        double bitsPerSecond = bytesPerSecond * 8;
 
         if (bitsPerSecond >= 1_000_000)
         {
-            float formatted = bitsPerSecond / 1_000_000.0f;
+            double formatted = bitsPerSecond / 1_000_000.0f;
             return $"{formatted:G3} Mbps";
         }
         if (bitsPerSecond >= 1_000)
         {
-            float formatted = bitsPerSecond / 1_000.0f;
+            double formatted = bitsPerSecond / 1_000.0f;
             return $"{formatted:G3} Kbps";
         }
-        return $"{bitsPerSecond} bps";
+        return $"{bitsPerSecond:G3} bps";
     }
 }
